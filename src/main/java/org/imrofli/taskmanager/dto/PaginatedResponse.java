8c1e93384ea048012d9dbd8a76abe6a52c974960
imrofli <imrofli@gmail.com>
2026-03-31 22:50:14 +0200

    init

--- src/main/java/org/imrofli/taskmanager/dto/PaginatedResponse.java
+++ src/main/java/org/imrofli/taskmanager/dto/PaginatedResponse.java
@@ -6,11 +6,4 @@ public record PaginatedResponse(
     List<TaskResponse> tasks,
     Long totalItems
 ) {}
-package org.imrofli.taskmanager.dto;
 
-import java.util.List;
-
-public record PaginatedResponse(
-    List<TaskResponse> tasks,
-    Long totalItems
-) {}
